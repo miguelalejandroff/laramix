d4cdb54be04e676b203e67b6463590ec944dde70
Miguel Fuentealba <64209145+miguelalejandroff@users.noreply.github.com>
2021-08-02 13:08:25 -0400

    Update UserProvider.php

--- src/Ifx/Auth/UserProvider.php
+++ src/Ifx/Auth/UserProvider.php
@@ -1,1 +1,39 @@
+<?php
 
+namespace Miguelalejandroff\Laravel\Ifx\Auth;
+
+use Illuminate\Auth\EloquentUserProvider;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Str;
+
+class UserProvider extends EloquentUserProvider
+{
+    /**
+     * Retrieve a user by the given credentials.
+     *
+     * @param array $statement
+     *
+     * @return Authenticatable|null
+     */
+    public function retrieveByCredentials(array $statement)
+    {
+        if (empty($statement)) {
+            return;
+        }
+
+        /*
+         * First we will add each credential element to the query as a where clause.
+         * Then we can execute the query and, if we found a user, return it in a
+         * Eloquent User "model" that will be utilized by the Guard instances.
+         */
+        $query = $this->createModel()->newQuery();
+
+        foreach ($statement as $key => $value) {
+            if (!Str::contains($key, 'password')) {
+                $query->whereRaw("upper({$key}) = upper(?)", [$value]);
+            }
+        }
+
+        return $query->first();
+    }
+}
